5cef4e7196cf60852bae52931ab6d738eda7fa87
Yann Birba <birbayann@gmail.com>
2024-04-22 22:48:48 +0200

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,46 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::{TcpListener, TcpStream}};
+use std::{
+    io::{BufRead, BufReader, BufWriter, Write},
+    net::{TcpListener, TcpStream},
+};
 
-fn handle_connection(mut stream: TcpStream) {
-    stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+fn handle_connection(mut reader: BufReader<TcpStream>, mut writer: BufWriter<TcpStream>) {
+    let mut request_string = String::new();
+    // we read_line and not read string 'cause read string read to EOF ... and it will never append ...
+    reader.read_line(&mut request_string).unwrap();
+    let path = request_string
+        .split("\r\n")
+        .nth(0)
+        .unwrap()
+        .split(" ")
+        .nth(1)
+        .unwrap();
+
+    if path == "/" {
+        writer.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+        return;
+    }
+    writer
+        .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+        .unwrap();
 }
 
 fn main() -> std::io::Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_connection(stream);
+    for rstream in listener.incoming() {
+        match rstream {
+            Ok(rstream) => {
+                let wstream = rstream.try_clone().unwrap();
+                let reader: BufReader<TcpStream> = BufReader::new(rstream);
+                let writer = BufWriter::new(wstream);
+                handle_connection(reader, writer)
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
     Ok(())
 }
