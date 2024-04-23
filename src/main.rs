67906dcd6eddccb7edadb148d5d5a21a264fac96
Yann Birba <birbayann@gmail.com>
2024-04-24 00:17:12 +0200

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -5,62 +5,86 @@ use std::{
 };
 
 fn handle_connection(mut reader: BufReader<TcpStream>, mut writer: BufWriter<TcpStream>) {
-    let mut request_string = String::new();
-    // we read_line and not read string 'cause read string read to EOF ... and it will never append ...
-    reader.read_line(&mut request_string).unwrap();
-    let path = request_string
-        .split("\r\n")
-        .nth(0)
-        .unwrap()
-        .split(" ")
-        .nth(1)
-        .unwrap();
+    // Base response is 404 until we handle it
+    let mut response = String::from("HTTP/1.1 404 Not Found\r\n\r\n");
+
+    let mut request_lines = Vec::new();
+
+    // Read all request lines until we found an empty line
+    loop {
+        let mut line = String::new();
+        match reader.read_line(&mut line) {
+            Ok(0) => break, // End
+            Ok(_) => {
+                if line.trim().is_empty() {
+                    break; // Empty line, end of request
+                }
+                request_lines.push(line.trim().to_string());
+            }
+            Err(err) => {
+                eprintln!("Erreur de lecture de la requête : {}", err);
+                return;
+            }
+        }
+    }
+
+    let path = request_lines.get(0).unwrap().split(" ").nth(1).unwrap();
 
     if path == "/" {
-        writer.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
-        return;
+        response = String::from("HTTP/1.1 200 OK\r\n\r\n");
     }
 
     let path_lenght = path.len();
 
     if path_lenght > 0 {
         let first_path = path.split("/").nth(1).unwrap();
-        if path.split("/").count() <= 2 || first_path == "echo" {
+
+        if path == "/user-agent" {
+            let user_agent_line = request_lines.iter().find(|&x| x.contains("User")).unwrap();
+
+            println!("{:?}", user_agent_line);
+
+            let text = user_agent_line.split(": ").last().unwrap();
+
+            println!("{:?}", text);
+
+            let content_lenght: usize = text.len();
+
+            response = format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",
+                    content_lenght,
+                    text
+                );
+        } else if path.split("/").count() <= 2 || first_path == "echo" {
             let text = if first_path == "echo" {
                 path.replace("/echo/", "")
             } else {
                 path.replace("/", "")
             };
 
-            let content_lenght = text.len();
+            let content_lenght: usize = text.len();
 
-            writer
-            .write(
-                format!(
+            response = format!(
                     "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",
                     content_lenght,
                     text
-                )
-                .as_bytes(),
-            )
-            .unwrap();
+                );
         }
     }
 
-    writer
-        .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
-        .unwrap();
+    // Send response to client
+    writer.write_all(response.as_bytes()).unwrap();
+    writer.flush().unwrap();
 }
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    for rstream in listener.incoming() {
-        match rstream {
-            Ok(rstream) => {
-                let wstream = rstream.try_clone().unwrap();
-                let reader: BufReader<TcpStream> = BufReader::new(rstream);
-                let writer = BufWriter::new(wstream);
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let reader = BufReader::new(stream.try_clone().unwrap());
+                let writer = BufWriter::new(stream.try_clone().unwrap());
                 handle_connection(reader, writer)
             }
             Err(e) => {
